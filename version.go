010dad69a0daa70e0cb539e97336e7cb9cc81c1d
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-12 12:48:45 -0800

    updated version to v0.0.7

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package webki
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.0.6"
+	Version = "v0.0.7"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "a2546e1"
+	GitCommit = "8609330"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-12 16:04"
+	VersionDate = "2023-12-12 20:48"
 )
